face9e73088d639af740d043b1d760cf655f8cd1
Christopher Laeri <claeri27@gmail.com>
2019-01-04 15:11:55 -0500

    Added coins scaffold and added association

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_04_192456) do
+ActiveRecord::Schema.define(version: 2019_01_04_200635) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "coins", force: :cascade do |t|
+    t.string "name"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_coins_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email"
@@ -22,4 +30,5 @@ ActiveRecord::Schema.define(version: 2019_01_04_192456) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "coins", "users"
 end
