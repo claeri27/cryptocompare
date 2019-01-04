2621c18932f333854235ce0b51d62a4943611aa8
Christopher Laeri <claeri27@gmail.com>
2019-01-04 17:02:33 -0500

    Installed knock, generated user_token_controller, changed application_controller, added before action fiter to CoinsController

--- app/controllers/coins_controller.rb
+++ app/controllers/coins_controller.rb
@@ -1,5 +1,6 @@
 class CoinsController < ApplicationController
   before_action :set_coin, only: [:show, :update, :destroy]
+  before_action :authenticate_user, only: [:create, :update, :destroy]
 
   # GET /coins
   def index
