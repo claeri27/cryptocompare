426bce5e1ae648fa11ebe6fa1e29c2ab72f69385
Christopher Laeri <claeri27@gmail.com>
2019-01-08 10:13:11 -0500

    Started work on display tables

--- client/src/components/CryptoList/CryptoList.js
+++ client/src/components/CryptoList/CryptoList.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import CryptoDetail from '../CryptoDetail/CryptoDetail.js';
 import CryptoCoin from '../CryptoCoin/CryptoCoin';
 import axios from 'axios';
+import './CryptoList.css';
 
 export default class CryptoList extends Component {
   constructor(props) {
@@ -33,8 +34,19 @@ export default class CryptoList extends Component {
 
   render() {
     return(
-      <div id="event-list">
-        <CryptoCoin coins={this.state.coins} />
+      <div id="crypto-list">
+        {this.state.coins.map(coin => (
+          <div className="coin-item-container">
+            <div className="coin-item" id="coin-name">{coin.name} </div>
+            <div className="coin-item" id="coin-symbol">{coin.symbol} </div>
+            <div className="coin-item" id="coin-price">{coin.current_price} </div>
+            <div className="coin-item" id="coin-volume">{coin.volume_24h} </div>
+            <div className="coin-item" id="coin-change-1hr">{coin.percent_change_1h} </div>
+            <div className="coin-item" id="coin-change-24hr">{coin.percent_change_24h} </div>
+            <div className="coin-item" id="coin-change-7d">{coin.percent_change_7d} </div>
+            <div className="coin-item" id="coin-market-cap">{coin.market_cap}</div>
+          </div>
+        ))}
       </div>
     )
   }
